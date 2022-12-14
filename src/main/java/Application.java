43d5f4a5184fa0aaf15ef58aaeb9a517e367e843
KarenNgugi <karenwngugi12@gmail.com>
2022-12-14 18:48:30 +0300

    maven project files

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+    private int b = 90;
+    public static void main(String[] args) {
+        int a;
+        System.out.println("Hiya!");
+
+        Application app = null;
+
+        System.out.println(app.b);
+    }
+}
